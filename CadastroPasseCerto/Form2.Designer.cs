78188b4746ba16bdbf1c9d8eff078633e609270d
Douglas Oliveira <douglasoliversil@gmail.com>
2018-10-13 00:34:22 -0300

    teste

--- CadastroPasseCerto/Form2.Designer.cs
+++ CadastroPasseCerto/Form2.Designer.cs
@@ -75,6 +75,7 @@
             this.Controls.Add(this.pictureBox1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form2";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Foto";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
