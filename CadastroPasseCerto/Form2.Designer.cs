477d5887b35a88f1d3828bdb3aa7e8282040e172
Douglas Oliveira <douglasoliversil@gmail.com>
2018-10-13 00:39:40 -0300

    eeeeeeee

--- CadastroPasseCerto/Form2.Designer.cs
+++ CadastroPasseCerto/Form2.Designer.cs
@@ -37,6 +37,9 @@
             // 
             // pictureBox1
             // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.pictureBox1.ErrorImage = null;
             this.pictureBox1.InitialImage = null;
             this.pictureBox1.Location = new System.Drawing.Point(12, 12);
